5077b2e84dfbee2ef623a636c8a28ab81e6b7ac7
Gavin Soko <sokolofg@gmail.com>
2018-08-21 03:58:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def count_elements(array)
   newArr = []
    array.each do |hash|
      newArr << {:name => hash[:name], :count => 1}
-     newArr.any? {|i|  
+     newArr.any? {|i| i 
    end
     newArr 
 end
